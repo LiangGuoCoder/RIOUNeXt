b6109d52dcd46814e65acb6c70160637d32c6a7a
LiangGuoCoder <146902502+LiangGuoCoder@users.noreply.github.com>
2023-11-04 18:40:36 +0800

    Update EncoderNet.py

--- nets/EncoderNet.py
+++ nets/EncoderNet.py
@@ -7,13 +7,22 @@ def odconv3x3(in_planes, out_planes, stride=1, reduction=0.0625, kernel_num=1):
     return ODConv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1,
                     reduction=reduction, kernel_num=kernel_num)
 
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, groups=groups, bias=False, dilation=dilation)
-
 def conv1x1(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
 
+
+class EcaLayer(nn.Module):
+    def __init__(self, channel, k_size=3):
+        super(EcaLayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
+        return x * y.expand_as(x)
 
 
 class Bottleneck(nn.Module):
@@ -25,23 +34,18 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
         if norm_layer is None:
             norm_layer = nn.BatchNorm2d
-        width = int(planes * (base_width / 64.)) * groups
-        self.conv1 = conv1x1(width, width)
-        self.bn1 = norm_layer(width)
-        # 利用3x3卷积进行特征提取
-        self.conv2 = conv3x3(width, width, stride, groups, dilation)
-        # self.conv2 = odconv3x3(inplanes, width, stride, reduction=reduction, kernel_num=kernel_num)
-        # self.conv2 = conv3x3(inplanes, width, stride, groups, dilation)
+        # width = int(planes * (base_width / 64.)) * groups
+        width = planes
+        self.conv1 = conv1x1(width, planes * self.expansion)
+        self.bn1 = norm_layer(planes * self.expansion)
+
+        self.conv2 = odconv3x3(inplanes, width, stride, reduction=reduction, kernel_num=kernel_num)
         self.bn2 = norm_layer(width)
-        # 利用1x1卷积上升通道数
-        self.conv3 = conv1x1(width, planes * self.expansion)
-        # self.conv3 = odconv1x1(width, planes * self.expansion, stride, reduction=reduction, kernel_num=kernel_num)
-        self.bn3 = norm_layer(planes * self.expansion)
+
+        self.conv3 = conv1x1(planes * self.expansion, inplanes)
+        self.bn3 = norm_layer(inplanes)
         self.PRelu = nn.PReLU(num_parameters=1)
         self.GeLU = nn.GELU()
-        # 添加一个并联的7x7卷积层
-        self.conv7x7 = nn.Conv2d(width, width, kernel_size=7, stride=1, padding=6, dilation=2, bias=False)
-        self.bn7x7 = norm_layer(width)
 
         self.downsample = downsample
         self.stride = stride
@@ -69,26 +73,17 @@ class Bottleneck(nn.Module):
 
         out = self.conv2(x)
         out = self.bn1(out)
-        # out = self.PRelu(out)
 
         out = self.conv1(out)
-        # out1 = self.conv7x7(out)
-        # out = out  + out1       # 在3X3卷积上并联一个7X7卷积核
-        # out = self.fuse_mode_1(out, out1)  # AFF
-        # out = self.bn2(out)
-        # out = self.PRelu(out)
+
         out = self.GeLU(out)
         out = self.conv3(out)
-        # out = self.bn3(out)
-        # out = self.eca(out)
 
         if self.downsample is not None:
             identity = self.downsample(x)
 
-        # out += identity
         out = self.fuse_mode(out, identity)  # iAFF
 
-        # out = self.PRelu(out)
         out = self.GeLU(out)
         return out
 
@@ -97,25 +92,28 @@ class ResNet(nn.Module):
 
         if k_size is None:
             k_size = [3, 3, 3, 3, 3]
-        self.inplanes = 64
+        self.inplanes = 16
         super(ResNet, self).__init__()
         assert fuse_type in ['AFF', 'iAFF', 'DAF']
         assert fuse_type_1 in ['AFF', 'iAFF', 'DAF']
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=3, bias=False)
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
         self.PRelu = nn.PReLU(num_parameters=1)
         self.pool = nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1)
 
-        # 512,512,64 -> 512,512,64-> 256,256,256
-        self.layer1 = self._make_layer(block, 64, layers[1], int(k_size[1]), stride=2, fuse_type=fuse_type,
+        self.layer0 = self._make_layer(block, 16, layers[0], int(k_size[0]), fuse_type=fuse_type,
                                        fuse_type_1=fuse_type_1, reduction=reduction, kernel_num=kernel_num)
-        # 256,256,64 -> 256,256,256-> 128,128,512
+
+        self.layer1 = self._make_layer(block, 64, layers[1], int(k_size[1]), fuse_type=fuse_type,
+                                       fuse_type_1=fuse_type_1, reduction=reduction, kernel_num=kernel_num)
+
         self.layer2 = self._make_layer(block, 128, layers[2], int(k_size[2]), stride=2, fuse_type=fuse_type,
                                        fuse_type_1=fuse_type_1, reduction=reduction, kernel_num=kernel_num)
-        # 128,128,256 -> 128,128,512-> 64,64,1024
+
         self.layer3 = self._make_layer(block, 256, layers[3], int(k_size[3]), stride=2, fuse_type=fuse_type,
                                        fuse_type_1=fuse_type_1, reduction=reduction, kernel_num=kernel_num)
-        # 64,64,512 -> 64,64,1024-> 32,32,2048
+
         self.layer4 = self._make_layer(block, 512, layers[4], int(k_size[4]), stride=2, fuse_type=fuse_type,
                                        fuse_type_1=fuse_type_1, reduction=reduction, kernel_num=kernel_num)
 
@@ -135,6 +133,7 @@ class ResNet(nn.Module):
         self.inplanes = planes * block.expansion
         self.fuse_mode_1 = iAFF(channels=512)
         self.fuse_mode_2 = iAFF(channels=1024)
+        self.fuse_mode_3 = iAFF(channels=2048)
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes, k_size=k_size, fuse_type=fuse_type, fuse_type_1=fuse_type_1,
                                 reduction=reduction, kernel_num=kernel_num))
@@ -147,7 +146,7 @@ class ResNet(nn.Module):
         x = self.bn1(x)
         feat1 = self.PRelu(x)
 
-        # x = self.pool(feat1)
+        x = self.pool(feat1)
         feat2 = self.layer1(x)
 
         feat_3 = self.layer2(feat2)
@@ -161,10 +160,8 @@ class ResNet(nn.Module):
         feat5 = self.layer4(feat4)
         return [feat1, feat2, feat3, feat4, feat5]
 
-
 def Net(fuse_type='iAFF', fuse_type_1='AFF', k_size=None, **kwargs):
     if k_size is None:
         k_size = [3, 3, 3, 3, 3]
-    model = ResNet(Bottleneck, [1, 1, 1, 1, 1], fuse_type=fuse_type, fuse_type_1=fuse_type_1, k_size=k_size, **kwargs)
+    model = ResNet(Bottleneck, [1, 1, 1, 3, 1], fuse_type=fuse_type, fuse_type_1=fuse_type_1, k_size=k_size, **kwargs)
     return model
-
